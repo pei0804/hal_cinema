1290fa8bfc074b68a94f1a96a76d4191a5e285a2
tikasan <satak47cpc@gmail.com>
2016-11-13 16:21:49 +0900

    add:　映画スケジュールの一覧表示

--- movieSchedule/movieScheduleList.php
+++ movieSchedule/movieScheduleList.php
@@ -1,7 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jumpei
- * Date: 2016/11/12
- * Time: 21:02
- */
+require_once $_SERVER['DOCUMENT_ROOT']. '/hal_cinema/classes/libs/Smarty.class.php';
+require_once $_SERVER["DOCUMENT_ROOT"]. '/hal_cinema/classes/Conf.php';
+require_once $_SERVER["DOCUMENT_ROOT"]. '/hal_cinema/classes/entity/MovieSchedule.class.php';
+require_once $_SERVER["DOCUMENT_ROOT"]. '/hal_cinema/classes/dao/MovieScheduleDAO.class.php';
+require_once $_SERVER["DOCUMENT_ROOT"]. '/hal_cinema/classes/Functions.php';
+
+$smarty = new Smarty();
+$smarty->setTemplateDir($_SERVER['DOCUMENT_ROOT'] . "/hal_cinema/templates/");
+$smarty->setCompileDir($_SERVER['DOCUMENT_ROOT'] . "/hal_cinema/templates_c/");
+$tplPath = "movieSchedule/list.tpl";
+
+$list = array();
+try {
+    $db = new PDO(DB_DNS, DB_USERNAME, DB_PASSWORD);
+    $movieScheduleDAO = new MovieScheduleDAO($db);
+    $list = $movieScheduleDAO->findByDate("2016-11-10");
+    $smarty->assign("list", $list);
+} catch (PDOException $ex) {
+    print_r($ex);
+    $smarty->assign("errorMsg", "DB接続に失敗しました。");
+    $tplPath = "error.tpl";
+} finally {
+    $db = null;
+}
+
+$smarty->display($tplPath);
